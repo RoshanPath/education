f5172903b772ef3c7cf131430849bd6147914aef
RoshanPathak <157211429+RoshanPath@users.noreply.github.com>
2024-03-08 02:42:47 +0530

    changes done

--- COMMON/header.js
+++ COMMON/header.js
@@ -3,7 +3,7 @@ document.write(`<!DOCTYPE html>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Educate</title>
     <link rel="stylesheet" href="index.CSS" />
     <link rel="stylesheet" href="indexResponsive.CSS">
     <link rel="stylesheet" href="COMMON/common.css">
